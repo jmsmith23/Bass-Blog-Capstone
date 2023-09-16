7d1520460dc45746504ace6d39d18b3922d581f0
Jeff Smith <127367482+jmsmith23@users.noreply.github.com>
2023-09-16 00:07:24 -0500

    Correct typos

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,6 +1,5 @@
 import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
