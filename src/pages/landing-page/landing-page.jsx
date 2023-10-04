2359584ba221b651fd71312e4d249d8020cf59d1
Jeff Smith <127367482+jmsmith23@users.noreply.github.com>
2023-10-03 20:22:55 -0500

    Restructure landing page to make the posts responsive with grid

--- src/pages/landing-page/landing-page.jsx
+++ src/pages/landing-page/landing-page.jsx
@@ -1,17 +1,23 @@
 import Carousel from '../../components/carousel/carousel';
 import Card from '../../components/card/card';
-import { Container, Stack, Box } from '@mui/material';
+import { Container, Grid, Box } from '@mui/material';
 
 const LandingPage = () => {
   return (
     <Container>
       <Carousel />
       <Box sx={{ flexGrow: 1 }}>
-        <Stack direction="row" spacing={0.5}>
-          <Card />
-          <Card />
-          <Card />
-        </Stack>
+        <Grid container my={3} spacing={1}>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card />
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card />
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card />
+          </Grid>
+        </Grid>
       </Box>
     </Container>
   );
