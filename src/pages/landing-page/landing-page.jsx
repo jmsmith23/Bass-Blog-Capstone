60e193d6f57b3c60192d82fd185bfee4cf25a261
Jeff Smith <127367482+jmsmith23@users.noreply.github.com>
2023-10-18 23:36:33 -0500

    Update style for landing page

--- src/pages/landing-page/landing-page.jsx
+++ src/pages/landing-page/landing-page.jsx
@@ -1,23 +1,46 @@
 import Card from '../../components/card/card';
-import { Container, Grid, Box } from '@mui/material';
+import { Container, Grid, Box, Paper } from '@mui/material';
 
 const LandingPage = () => {
   return (
-    <Container>
-      <Box sx={{ flexGrow: 1 }}>
-        <Grid container my={3} spacing={1}>
-          <Grid item xs={12} sm={6} md={4}>
-            <Card />
+    <>
+      <Paper
+        elevation={0}
+        sx={{
+          backgroundImage: 'url(https://i.imgur.com/919ix4p.jpg)',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          minHeight: '100dvh',
+        }}
+      ></Paper>
+      <Container>
+        {/* <Box
+        component="img"
+        sx={{
+          height: 233,
+          width: 350,
+          maxHeight: { xs: 233, md: 167 },
+          maxWidth: { xs: 350, md: 250 },
+        }}
+        alt="J Smith Bass"
+        src="https://i.imgur.com/919ix4p.jpg"
+      /> */}
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container my={3} spacing={1}>
+            <Grid item xs={12} sm={6} md={4}>
+              <Card />
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+              <Card />
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+              <Card />
+            </Grid>
           </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <Card />
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <Card />
-          </Grid>
-        </Grid>
-      </Box>
-    </Container>
+        </Box>
+      </Container>
+    </>
   );
 };
 
