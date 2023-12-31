2e7737b36fc61f64c83a923a06f8d21ff3f9e78d
Jeff Smith <127367482+jmsmith23@users.noreply.github.com>
2023-12-30 22:24:57 -0600

    Style post page with MUI

--- src/pages/new-post-page/new-post-page.jsx
+++ src/pages/new-post-page/new-post-page.jsx
@@ -1,10 +1,50 @@
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import { Box, Container } from '@mui/material';
+import Stack from '@mui/material/Stack';
+
 const NewPost = () => {
   return (
     <>
-      <form>
-        Title
-        <input type="text" />
-        {/* 
+      <Typography variant="h3" sx={{ textAlign: 'center', my: 6 }}>
+        New Post
+      </Typography>
+      <Container
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          mb: 8,
+        }}
+        maxWidth="normal"
+      >
+        <Box
+          component="form"
+          sx={{
+            '& .MuiTextField-root': { mb: 1, width: '100%' },
+            width: '50%',
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <Stack spacing={3}>
+            <TextField
+              label="Title"
+              id="outlined-size-normal"
+              sx={{ mb: 20 }}
+            />
+            <TextField
+              id="outlined-multiline-flexible"
+              label="Description"
+              multiline
+              rows={4}
+            />
+          </Stack>
+        </Box>
+      </Container>
+
+      {/* 
         TODO:
         - complete this form (add a textarea)
         - change everything to Material if you want
@@ -12,7 +52,6 @@ const NewPost = () => {
             - NOTE: Uploading the file will be hard
         - create a back-end route for creating a new post
          */}
-      </form>
     </>
   );
 };
